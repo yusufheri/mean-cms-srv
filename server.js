afe79de1c2812c483924b61b9b733ddd217b3e12
YUSUF HERI <yusufheri64@gmail.com>
2019-09-08 13:12:00 +0200

    Fix server.js

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+
 let express = require('express');
+
+
 let cors = require('cors');
 let router = require('./api/v1/index');
+const mongoose = require('mongoose');
 
+const connection = mongoose.connection;
 let app = express();
 app.set('port',( process.env.PORT || 4000));
+app.use('/api/v1', router);
 
 //  Middleware
 app.use(cors());
@@ -11,9 +17,17 @@ app.use((req, res, next) => {
 	console.log(`req is handked at ${new Date()}`);
 	next();
 });
-app.use('/api/v1', router);
 
-app.listen(app.get('port'), () => {
-	const port = app.get('port');
-	console.log('Express is listen on port' + (port) + ' !!');
+mongoose.connect('mongodb://localhost/meancms', {useNewUrlParser: true});
+connection.on('error', (err) => {
+	console.error(`Connection to MongoDB error : ${err.message}`);
+});
+
+connection.once('open', () => {
+	console.error('Connection to MongoD');
+    
+	app.listen(app.get('port'), () => {
+		const port = app.get('port');
+		console.log('Express is listen on port' + (port) + ' !!');
+	});
 });
